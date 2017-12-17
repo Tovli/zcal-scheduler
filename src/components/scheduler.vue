e2f61a8a1ba4193a49a5d1e704cb172ea61dabfe
eliranmal <eliranmal@gmail.com>
2017-12-17 17:59:26 +0200

    - add week padding - update styles

--- src/components/scheduler.vue
+++ src/components/scheduler.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="scheduler">
-    <div v-for="day in days" class="day">
-      <span>{{day.header}}</span>
+    <div v-for="day in days" class="day" v-bind:class="{ isToday: day.isToday, isEmpty: day.pad }">
+      <span class="date">{{day.date}}</span>
+      <span class="dayOfTheWeek">{{day.dayOfTheWeek}}</span>
     </div>
   </div>
 </template>
 
 <script>
-  import { daysInMonth, dayOfTheWeek } from '../services/time-ranger';
+  import { WEEK_DAYS, dayCountInMonth, dayOfTheWeek } from '../services/time-ranger';
 
   export default {
     name: 'scheduler',
     data() {
       const now = new Date();
-      const dayCount = daysInMonth(now);
+      const dayCount = dayCountInMonth(now);
       const days = Array.of(...(new Array(dayCount + 1).keys()))
         .slice(1, dayCount + 1)
-        .map(i => ({ header: `${i} ${dayOfTheWeek(now, i)}` }));
+        .map((date) => {
+          const entry = {
+            date,
+            dayOfTheWeek: dayOfTheWeek(now, date),
+            isToday: now.getDate() === date,
+          };
+          return entry;
+        });
+
+      const firstDayIndex = WEEK_DAYS.findIndex(day => day === days[0].dayOfTheWeek);
+      const lastDayIndex = WEEK_DAYS.findIndex(day => day === days[days.length - 1].dayOfTheWeek);
+      // pad with empty blocks, for week alignment
+      for (let i = 0; i < firstDayIndex; i += 1) {
+        days.unshift({ pad: true });
+      }
+      for (let i = lastDayIndex + 1; i < 7; i += 1) {
+        days.push({ pad: true });
+      }
+
       return {
         days,
       };
@@ -27,24 +46,63 @@
 <style scoped>
 
   .scheduler {
-    border-top: 1px solid skyblue;
-    border-left: 1px solid skyblue;
+    border-top: 1px solid lightskyblue;
+    border-left: 1px solid lightskyblue;
     width: 100%;
     height: 100%;
   }
 
   .day {
+    position: relative;
     float: left;
     display: inline-block;
     text-align: left;
-    /*border: 1px solid green;*/
     width: calc(100% / 7);
-    height: calc(100% / 5);
+    height: calc(100% / 6);
     padding: 1em;
     /*margin: 1em;*/
-    background-color: aliceblue;
-    border-bottom: 1px solid dodgerblue;
-    border-right: 1px solid dodgerblue;
+    background-color: #fafafa;
+    border-bottom: 1px solid skyblue;
+    border-right: 1px solid skyblue;
+  }
+
+  .day.isEmpty:before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #eee;
+  }
+
+  .day .date {
+    display: inline-block;
+    padding: .4em 0;
+    width: 1.8em;
+    height: 1.8em;
+    border-radius: 50%;
+    font-size: 80%;
+    line-height: 1.2;
+    text-align: center;
+    font-weight: bold;
+    color: #fff;
+    background-color: #ccc;
+  }
+
+  .day .dayOfTheWeek {
+    color: #999;
+  }
+
+  .day.isToday .date {
+    /*background-color: dodgerblue;*/
+    background-color: lightskyblue;
+  }
+
+  .day.isToday .dayOfTheWeek {
+    color: dodgerblue;
+    /*color: lightskyblue;*/
   }
 
 </style>
--- src/services/time-ranger.js
+++ src/services/time-ranger.js
@@ -1,17 +1,30 @@
-// wat
+
+const WEEK_DAYS = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+];
 
 const parse = (ceil = { unit: 'month', items: 3 },
                floor = { unit: 'day' },
                strategy = /around|before|after/,
                now = new Date()) => {
+// eslint-disable-next-line no-console
   console.log('ceil', ceil);
+// eslint-disable-next-line no-console
   console.log('floor', floor);
+// eslint-disable-next-line no-console
   console.log('strategy', strategy);
+// eslint-disable-next-line no-console
   console.log('now', now);
   throw new Error('not implemented yet');
 };
 
-function daysInMonth(date) {
+function dayCountInMonth(date) {
   const d = new Date(date);
   d.setMonth(d.getMonth() + 1);
   d.setDate(0);
@@ -21,20 +34,13 @@ function daysInMonth(date) {
 function dayOfTheWeek(date, dayOfMonth) {
   const d = new Date(date);
   d.setDate(dayOfMonth);
-  return [
-    'sunday',
-    'monday',
-    'tuesday',
-    'wednesday',
-    'thursday',
-    'friday',
-    'saturday',
-  ][d.getDay()];
+  return WEEK_DAYS[d.getDay()];
 }
 
 
 export {
+  WEEK_DAYS,
   parse,
-  daysInMonth,
+  dayCountInMonth,
   dayOfTheWeek,
 };
